9f5832bef2c3d3489975eea79a0b049bd66a76f6
Miller <Miller@MacBook-Pro.local>
2018-01-24 18:25:26 -0800

    added page two functionality

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-constructor(props){
-  super(props)
-  this.state = {
-    pageTwoDisplay: 'none',
-    questions: [{}]
+  constructor(props) {
+    super(props)
+    this.state = {
+      pageTwoDisplay: 'none',
+      questions: [{}]
+    }
+    this.startButton = this.startButton.bind(this);
+    this.addQuestionButton = this.addQuestionButton.bind(this);
+    this.pageTwoNext = this.pageTwoNext.bind(this);
   }
-  this.startButton = this.startButton.bind(this);
-}
 
-startButton(){
-  document.getElementById('pageOne').style.display = 'none';
-  this.setState({
-    pageTwoDisplay: ''
-  })
-}
+
+  startButton() {
+    document.getElementById('pageOne').style.display = 'none';
+    this.setState({
+      pageTwoDisplay: ''
+    })
+  }
+
+  addQuestionButton() {
+    let inpt = document.createElement('input');
+    inpt.className = 'questionInput'
+    document.getElementById('questionSection').appendChild(inpt);
+  }
+
+  pageTwoNext(){
+    
+  }
   render() {
     return (
       <div>
@@ -29,9 +42,18 @@ startButton(){
           </button>
           </div>
         </div>
-        <div style={{display: this.state.pageTwoDisplay}} className='pageTwo'>
-          <h1 style={{ textAlign: 'center', fontSize: 40 + 'px'}} className='title'>Enter the questions for your trivia here!</h1>
-          <input className='questionInput'/>
+        <div style={{ display: this.state.pageTwoDisplay }} className='pageTwo'>
+          <h1 style={{ textAlign: 'center', fontSize: 40 + 'px' }} className='title'>Enter the questions for your trivia here!</h1>
+          <div id='questionSection'>
+            <input className='questionInput' placeholder='1' />
+            <input className='questionInput' placeholder='2' />
+            <input className='questionInput' placeholder='3' />
+            <input className='questionInput' placeholder='4' />
+            <input className='questionInput' placeholder='5' />
+            <input className='questionInput' placeholder='6' />
+          </div>
+          <button onClick={this.addQuestionButton} className='questionInput'> Click here to add Additional Questions!</button>
+          <button onClick={this.pageTwoNext} className='questionInput'>Click here to continue!</button>
         </div>
       </div>
     );
